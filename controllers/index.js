8662eaeee5e853fb7bc19498e60e906cb92869ce
Chaz Coats <chazcoats2@hotmail.com>
2023-09-18 14:16:09 -0400

    working on database models. adding details to rout

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,4 @@
-const router = require('express').router;
+const router = require('express').router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 const dashboardRoutes = require('./dashboardRoutes');
