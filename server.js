bf4899c7b1b822a2cb84ff79d5b5b5a078c8eaed
Chaz Coats <chazcoats2@hotmail.com>
2023-09-20 14:37:30 -0400

    writing date function. Importing server modules

--- server.js
+++ server.js
@@ -4,3 +4,5 @@ const sequelize = require('./config/connection');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const helpers = require('./utils/helpers');
+const routes = require('./controllers');
+const hbs = exphbs.create({helpers});
